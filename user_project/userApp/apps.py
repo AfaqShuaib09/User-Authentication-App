b91c46851de653f894b9cde85ee460cfecde7068
MuhammadAfaqShuaib <afaq.shoaib09@gmail.com>
2022-08-04 12:56:57 +0500

    feat(some improvements):
      1. Login user upon successful signup
      2. Allow editing email in profile too
      3. Allow single field update
      4. Increase address character limit
      5. Help text improved
      6. Add radio buttons for gender

--- user_project/userApp/apps.py
+++ user_project/userApp/apps.py
@@ -2,6 +2,9 @@
 from django.apps import AppConfig
 
 
-class UserappConfig(AppConfig):
+class UserAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userApp'
+
+    def ready(self):
+        import userApp.signals
