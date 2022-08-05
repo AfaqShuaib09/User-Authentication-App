6d37d91c56edc969732539d6f71d24f50c1ac4f9
MuhammadAfaqShuaib <afaq.shoaib09@gmail.com>
2022-08-05 15:26:52 +0500

    django admin customize to remove user field in profile

--- user_project/userApp/admin.py
+++ user_project/userApp/admin.py
@@ -1,4 +1,4 @@
-''' Admin views for User App '''
+''' Admin Panel View for User App '''
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
@@ -17,17 +17,19 @@ class CustomProfileAdmin(admin.ModelAdmin):
                 'address', 'image', 'get_profile_image']
     search_fields = ['user__username', 'full_name']
     radio_fields = {'gender': admin.HORIZONTAL}
-    readonly_fields=('get_profile_image', )
+    readonly_fields=('user', 'get_profile_image')
 
     def edit_details(self, obj):
+        ''' Link to edit the profile of the user '''
         return format_html(f'<a href="/admin/userApp/profile/{obj.id}/change/">Edit</a>')
 
     def get_profile_image(self, obj):
+        ''' Returns the profile image of the user as a field '''
         return format_html(f'<image src="/media/{obj.image}" style="width:150px;height:100px;">')
 
 
 class ProfileInline(admin.StackedInline):
-    ''' Inline Profile Model associated with the User Model '''
+    ''' Inline Profile Model associated with the User '''
     model = Profile
     can_delete = False
     verbose_name_plural = 'Profile'
@@ -40,7 +42,7 @@ class CustomUserAdmin(UserAdmin):
                         'date_joined', 'last_login', 'edit_details')
     list_select_related = ('profile', )
     fieldsets = [
-        ('Personal Information', {'fields': ['username', 'password', 'email', 'is_staff', 'is_superuser',
+        ('Personal Information', {'fields': ['username' ,'email', 'is_staff', 'is_superuser',
             'is_active', 'date_joined', 'last_login']}),
     ]
     readonly_fields=('username', )
@@ -50,10 +52,12 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ['is_staff', 'is_superuser', 'is_active', 'date_joined', 'last_login']
 
     def get_gender(self, instance):
+        ''' Returns the gender of the user '''
         return instance.profile.gender
     get_gender.short_description = 'Gender'
 
     def edit_details(self, obj):
+        ''' Link to edit the details of the user '''
         return format_html(f'<a href="/admin/auth/user/{obj.id}/change/">Edit</a>')
 
     def get_inline_instances(self, request, obj=None):
